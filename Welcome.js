fbf30692f00edcb3df040631d6da8e3246e362b9
Jasleen Kaur <jka349@sfu.ca>
2025-03-09 03:34:56 -0700

    updated welcome page

--- Welcome.js
+++ Welcome.js
@@ -1,26 +1,27 @@
 import React from "react";
+import "./welcome.css"; // Ensure the path is correct in your setup
 
-
-function gameStart()
-{
-    return (
-        <div className="started-game-trial">
-            <h1>Started</h1>
-        </div>
-    )
+function gameStart() {
+  return (
+    <div className="started-game-trial">
+      <h1>Started</h1>
+    </div>
+  );
 }
 
-export default function Welcome()
-{
+export default function Welcome() {
+  return (
+    <div className="welcome-page">
+      <div className="welcome-story-box">
+        {/* The image goes here, pinned to the bottom-right via CSS */}
+        <img src="./blood-drop.png" alt="Blood Drop" className="blood-drop" />
+        <img src="./blood-left.jpg" alt="Blood Left" className="blood-left" />
 
-    return (
-        <div className="welcome-page">
-            <div className="welcome-story-box">
-                <h1 className="welcome-text">
-                   KILLSWITCH
-                </h1>   
-                <button className="welcome-start-button" onClick={gameStart}>start</button>
-            </div>
-        </div>
-    )
+        <h1 className="welcome-text">KILLSWITCH</h1>
+        <button className="welcome-start-button" onClick={gameStart}>
+          start
+        </button>
+      </div>
+    </div>
+  );
 }
