c460bfb954be7537265c5825a489fa4f92fe1dba
Puja Shah <pks37@sfu.ca>
2025-03-09 04:07:40 -0700

    guess form

---
+++ Victory.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Victory(props) {
+  return (
+    <div className='victory-main'>
+      <h1 className='victory-text'>You WIN</h1>
+    </div>
+  )
+}
+
+
+export default Victory
