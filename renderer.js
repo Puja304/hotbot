f8f29aa3be1722f34e5ee72c12534fd98288bb25
Jasleen Kaur <jka349@sfu.ca>
2025-03-13 15:29:29 -0700

    welcome

--- renderer.js
+++ renderer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Welcome from "./Welcome"
+import Welcome from "./Welcome";
 import MainInterface from "./MainInterface";
 import CaseDetails from "./CaseDetails";
 import Guess from "./Guess";
-import "./welcome.css"
-
-import './style.css';  
+import "./welcome.css";
+import "./style.css";
 
 const bgMusic = new Audio("./music/haunted-forest-309396.mp3");
 bgMusic.loop = true;
@@ -16,19 +15,10 @@ bgMusic.play();
 const App = () => {
     return (
         <div className="app-frame">
-            {/* <Guess
-            killer="killer"
-            weapon="weapon"
-            location="location" /> */}
-<<<<<<< HEAD
             <Welcome />
-=======
-            <Guess />
->>>>>>> 5b9a1de934b4ed52722f73350a5c2b4d66beff1b
         </div>
     );
 };
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
+root.render(<App />);
